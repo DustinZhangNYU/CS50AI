c95055c86e7a3cfaf36eae6c8d82c95ff1c454f0
DustinZhangNYU <91435718+DustinZhangNYU@users.noreply.github.com>
2024-01-20 07:31:18 -0500

    finish project0-ttt

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -9,18 +9,11 @@ X = "X"
 O = "O"
 EMPTY = None
 
-MAXPLAYER = True
-
-moves = set((i,j) for i in range(3) for j in range(3))
 
 def initial_state():
     """
     Returns starting state of the board.
     """
-    global MAXPLAYER,moves
-    MAXPLAYER = True
-
-    moves = set((i, j) for i in range(3) for j in range(3))
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
@@ -30,15 +23,18 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    if MAXPLAYER:
+    if count_for_X(board) <= count_for_O(board):
         return X
-
     return O
 
 
 def actions(board):
-
-    return actions
+    moves = set()
+    for i in range(3):
+        for j in range(3):
+            if(board[i][j] == EMPTY):
+                moves.add((i,j))
+    return moves
     """
     Returns set of all possible actions (i, j) available on the board.
     """
@@ -46,16 +42,12 @@ def actions(board):
 
 
 def result(board, action):
-    global MAXPLAYER
-    curPlayer = O
-    if MAXPLAYER:
-        curPlayer = X
+    if action[0] < 0 or action[0] >= 3 or action[1] < 0 or action[1] >=3 or board[action[0]][action[1]] != EMPTY:
+        raise Exception("illegal move")
+    p = player(board)
     """
     Returns the board that results from making move (i, j) on the board."""
-    board[action[0]][action[1]] = curPlayer
-
-    moves.remove(action)
-    MAXPLAYER = not MAXPLAYER
+    board[action[0]][action[1]] = p
     return board
 
 def winner(board):
@@ -64,10 +56,10 @@ def winner(board):
     """
 
     for i in range(3):
-        if (board[i][0] == board[i][1] and board[i][1] == board[i][2]):
+        if (board[i][0] != EMPTY and board[i][0] == board[i][1] and board[i][1] == board[i][2] ):
             return board[i][0];
     for i in range(3):
-        if (board[0][i] == board[1][i] and board[1][i] == board[2][i]):
+        if (board[0][i] != EMPTY and board[0][i] == board[1][i] and board[1][i] == board[2][i]):
             return board[0][i];
     if (board[0][0] == board[1][1] and board[1][1] == board[2][2]):
         return board[1][1];
@@ -83,11 +75,25 @@ def terminal(board):
     """
     if winner(board):
         return True
-    if len(moves) == 0:
+    if count_for_X(board) + count_for_O(board) == 9:
         return True
     return False
 
+def count_for_X(board):
+    count = 0
+    for row in board:
+        for e in row:
+            if(e == X):
+                count += 1
+    return count
 
+def count_for_O(board):
+    count = 0
+    for row in board:
+        for e in row:
+            if(e == O):
+                count += 1
+    return count
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
@@ -102,23 +108,20 @@ def utility(board):
 
 
 def minimax(board):
-    if(terminal(board)):
-        return None
     """
     Returns the optimal action for the current player on the board.
     """
-    result = minmaxHelper(board, MAXPLAYER)[1]
-    return result
-def minmaxHelper(board, Maxing):
+    res = minimaxHelper(board)[1]
+    return res
+def minimaxHelper(board):
     if (terminal(board)):
         return (utility(board),None)
-    if(Maxing):
+    if(player(board) == 'X'):
         bestScore = -100
         bestAction = None
-        for (i, j) in moves:
+        for (i, j) in actions(board):
             board[i][j] = X
-            moves.remove((i,j))
-            score,Action = minmaxHelper(board, not Maxing)
+            score, action = minimaxHelper(board)
             # if (score == bestScore):
             #     k = random.randint(0, 1)
             #     if(k == 0):
@@ -127,16 +130,14 @@ def minmaxHelper(board, Maxing):
             if(score > bestScore):
                 bestScore = score
                 bestAction = (i,j)
-            moves.add((i,j))
             board[i][j] = EMPTY
         return (bestScore, bestAction)
     else:
         bestScore = 100
         bestAction = None
-        for (i, j) in moves:
+        for (i, j) in actions(board):
             board[i][j] = O
-            moves.remove((i, j))
-            score, Action = minmaxHelper(board, not Maxing)
+            score, Action = minimaxHelper(board)
             # if (score == bestScore):
             #     k = random.randint(0, 1)
             #     if(k == 0):
@@ -144,11 +145,13 @@ def minmaxHelper(board, Maxing):
             #         bestAction = (i, j)
             if (score < bestScore):
                 bestScore = score
-                bestAction = (i, j)
-            moves.add((i, j))
+                bestAction = (i,j)
             board[i][j] = EMPTY
         return (bestScore, bestAction)
 
 
-board = initial_state()
+
+board = [[EMPTY, X, EMPTY],
+            [EMPTY, X, O],
+            [EMPTY, EMPTY, EMPTY]]
 
